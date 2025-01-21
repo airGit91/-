240a6ba5218d3ea47b305a49e844e18ab2a495f4
Nik <airgit1991@rambler.ru>
2025-01-21 20:21:33 +0300

    Final commit

--- Копилка.cpp
+++ Копилка.cpp
@@ -1,31 +1,21 @@
-﻿// Копилка.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
 
 int main()
 {
     setlocale(0, "");
         //Задача 1. Симулятор маршрутки
+  
     int price = 20;
-    int salary = price * 0.25;
-    int fuel = price * 0.2;
-    int taxes = price * 0.2;
-    int repairs = price * 0.2;
-    int total = price - (salary + fuel + taxes + repairs);
     int sum = 0;
-    int finalCost = 0;
- 
     int passengers = 0;
     int entered;
     int left;
-
+ 
     std::string sationFirst = "Улица программистов";
     std::string secondStation = "Проспект алгоритмов";
     std::string thirdStation = "Андроидов переулок";
     std::string fourthStation = "Площадь фулстека";
 
-    //std::cout << price << "\n" << salary << "\n" << fuel << "\n" << taxes << "\n" << repairs << "\n";
     std::cout << "Прибываем на остановку " << sationFirst << ".В салоне пассажиров : " << passengers << "\n";
 
     std::cout << "Сколько пассажиров вышло на остановке?";
@@ -36,8 +26,7 @@ int main()
     std::cout << "Сколько пассажиров зашло  на остановке?";
     std::cin >> entered;
     passengers += entered;
-    sum = sum + (total * entered);
-    finalCost += finalCost + (price * entered);
+    sum = sum + (price * entered);
     std::cout << "\n";
 
     std::cout << "Отправляемся с остановки " << sationFirst << ".В салоне пассажиров : " << passengers << "\n";
@@ -53,8 +42,7 @@ int main()
     std::cout << "Сколько пассажиров зашло  на остановке?";
     std::cin >> entered;
     passengers += entered;
-    sum =  sum + (total * entered);
-    finalCost += finalCost + (price * entered);
+    sum = sum + (price * entered);
     std::cout << "\n";
 
     std::cout << "Отправляемся с остановки " << secondStation << ".В салоне пассажиров : " << passengers << "\n";
@@ -70,8 +58,7 @@ int main()
     std::cout << "Сколько пассажиров зашло  на остановке?";
     std::cin >> entered;
     passengers += entered;
-    sum = sum + (total * entered);
-    finalCost += finalCost + (price * entered);
+    sum = sum + (price * entered);
     std::cout << "\n";
 
     std::cout << "Отправляемся с остановки " << thirdStation << ".В салоне пассажиров : " << passengers << "\n";
@@ -87,30 +74,55 @@ int main()
     std::cout << "Сколько пассажиров зашло  на остановке?";
     std::cin >> entered;
     passengers += entered;
-    sum = sum + (total * entered);
-    finalCost += finalCost + (price * entered);
+    sum = sum + (price * entered);
     std::cout << "\n";
 
     std::cout << "Отправляемся с остановки " << fourthStation << ".В салоне пассажиров : " << passengers << "\n";
     std::cout << "-----------Едем---------" << "\n";
+
+    int salary = sum * 0.25;
+    int fuel = sum * 0.2;
+    int taxes = sum * 0.2;
+    int repairs = sum * 0.25;
+    int total = total = sum - (salary + fuel + taxes + repairs);
+
     
- /*   Всего заработали : 100 руб.
+    std::cout << "Всего заработали: " << sum << " руб.\n";
+    std::cout << "Зарплата водителя: " << salary << " руб.\n";
+    std::cout << "Расходы на топливо: " << fuel << " руб.\n";
+    std::cout << "Налоги: " << taxes << " руб.\n";
+    std::cout << "Расходы на ремонт машины: " << repairs << " руб.\n";
+    std::cout << "Итого доход: " << total << " руб.\n";
 
-         : 25 руб.
+    //Задача 2. Обмен местами
 
-         : 20 руб.
+    int a = 42;
+    int b = 153;
+    int c = 0;
+    std::cout << "a: " << a << "\n"; 
+    std::cout << "b: " << b << "\n"; 
+    c = a; 
+    b += c; 
+    a = b - c; 
+    b = c; 
+    std::cout << "a: " << a << "\n";  
+    std::cout << "b: " << b << "\n"; 
 
-         : 20 руб.
 
-         : 20 руб.
+    //Задача 3. Злостные вредители
+    
+    int beginHeight = 100;
+    int dailyGrowth = 50;
+    int nightFade = 20;
+    int finalHeight = beginHeight + (2.5 * (dailyGrowth - nightFade));
+ 
+    //Задача 4 (дополнительная). Повышаем градус сложности*
 
-         : 15 руб.*/
-    std::cout << "Всего заработали :" << finalCost << "\n";
-    std::cout << "Расходы на топливо :" << sum << "\n";
-    std::cout << "Расходы на ремонт машины :" << sum << "\n";
-    std::cout << "Налоги :" << sum << "\n";
-    std::cout << "Итого доход :" << sum << "\n";
-
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    std::cout << "a: " << a << "\n"; 
+    std::cout << "b: " << b << "\n"; 
 }
 
 
@@ -118,31 +130,3 @@ int main()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
